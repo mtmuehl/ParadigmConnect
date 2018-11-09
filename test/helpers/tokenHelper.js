433e179f2fd8930c8e0761b1492d771a9307d130
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-11-09 16:35:28 -0600

    Updating token helper for new dependency

--- test/helpers/tokenHelper.js
+++ test/helpers/tokenHelper.js
@@ -1,10 +1,10 @@
 const SimpleERC20 = require('simple-erc20');
-const TokenContractInfo = require('paradigm-core-solidity/build/contracts/Token');
+const TokenContractInfo = require('paradigm-protocol-contracts/build/contracts/DIGMToken');
 
 module.exports = async (variableName, tokenName, tokenSymbol, from) => {
 
   const TokenContract = new web3.eth.Contract(TokenContractInfo.abi);
-  const DeployedContract = await TokenContract.deploy({ data: TokenContractInfo.bytecode , arguments: [tokenName, tokenSymbol] }).send({ from, gas: 4500000 });
+  const DeployedContract = await TokenContract.deploy({ data: TokenContractInfo.bytecode , arguments: [] }).send({ from, gas: 4500000 });
 
   global[variableName.toUpperCase()] = DeployedContract._address;
   global[variableName.toLowerCase()] = SimpleERC20(DeployedContract._address, await web3.eth.net.getId(), web3);
